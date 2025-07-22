a6f105d67fffa0d5f6f16680442aa955ad850d5a
AMKolev22 <amkolev22@codingburgas.bg>
2025-07-22 18:14:59 +0300

    Add skeleton template for user dashboard

--- web/app/dashboard/user/page.tsx
+++ web/app/dashboard/user/page.tsx
@@ -1,20 +1,51 @@
+"use client"
+
 import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { Typewriter } from "@/components/Typewriter";
+import { ScrollArea } from "@/components/ui/scroll-area";
 export default function Page() {
+
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    let email = Cookies.get('email');
+    if (!email) return;
+
+    const getUser = async () => {
+      try {
+        const res = await fetch('/api/userData', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ email }),
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        if (!res.ok) {
+          console.error(data.error);
+          return;
+        }
+
+        setName(data.user.name);
+      } 
+      catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUser();
+  }, []);
+
+
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -22,28 +53,20 @@ export default function Page() {
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    routing here
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+
+        <div className="flex-1 rounded-xl bg-muted/50 p-4 md:h-auto ml-4 mr-4 mb-4">
+            <div className="flex items-center justify-between mb-6">
+              <span className="font-semibold text-xl mt-4 ml-4">
+                <Typewriter bold={true} text={`Welcome back, ${name}.`} speed={60} />
+              </span>
+            </div>
+            <div className="flex font-semibold justify-between mt-16">
+              <h1 className="ml-4">Here's an overview of your borrowed items</h1>
+              <h1 className="mr-4">Here's the status of your 3 most recent requests</h1>
+            </div>
+          {/* <h1>You have X pending, Y, acceppted, Z denied requests.</h1> */}
         </div>
       </SidebarInset>
     </SidebarProvider>
