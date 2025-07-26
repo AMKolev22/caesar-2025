b9c9f58257319c25937db8d9898aa8539556147a
AMKolev22 <amkolev22@codingburgas.bg>
2025-07-26 19:17:03 +0300

    Give each page a SSR-ed layout and title

---
+++ web/app/dashboard/manager/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Caesar | Overview',
+};
+
+ 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+ return (
+    <html lang="en">
+        <body><main>{children}</main></body>
+    </html>
+  )
+}
---
+++ web/app/dashboard/user/myRequests/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Caesar | My Requests',
+};
+
+ 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+ return (
+    <html lang="en">
+        <body><main>{children}</main></body>
+    </html>
+  )
+}
