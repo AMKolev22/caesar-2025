1ad87d9de4c78e282d459764d2ded5ac4cd47fa9
AMKolev22 <amkolev22@codingburgas.bg>
2025-07-27 12:12:20 +0300

    Fix typewriter component logic

--- web/src/components/Typewriter.tsx
+++ web/src/components/Typewriter.tsx
@@ -1,32 +1,28 @@
 import { useEffect, useState } from "react";
 
-export const Typewriter = ({ text, speed, bold } : {text : string, speed : number, bold: boolean}) => {
+export const Typewriter = ({ text, speed, bold }: { text: string, speed: number, bold: boolean }) => {
   const [displayText, setDisplayText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
-    useEffect(() => {
-    let intervalId;
 
-  if (currentIndex < text.length) {
-    intervalId = setInterval(() => {
-      setDisplayText((prevText) => prevText + text[currentIndex]);
-      setCurrentIndex((prevIndex) => prevIndex + 1);
+  useEffect(() => {
+    setDisplayText('');
+    setCurrentIndex(0);
+  }, [text]);
 
-      if (currentIndex === text.length - 1) {
-        clearInterval(intervalId);
-      }
-    }, speed);
-  }
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeoutId = setTimeout(() => {
+        setDisplayText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, speed);
+      
+      return () => clearTimeout(timeoutId);
+    }
+  }, [currentIndex, text, speed]);
 
-  return () => clearInterval(intervalId);
-}, [currentIndex, text, speed]);
-return (
-  <>
-    {bold && (
-      <span id="anim" className="bold">{displayText}</span>
-    )}
-    {!bold && (
-      <span id="anim" className="font-normal">{displayText}</span>
-    )}
-  </>
-);
+  return (
+    <span className={bold ? "font-bold" : "font-normal"}>
+      {displayText}
+    </span>
+  );
 };
