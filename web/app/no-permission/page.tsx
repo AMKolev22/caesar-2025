a43ed386b83f0d3bc7395517c05dd31cdf921c14
AMKolev22 <amkolev22@codingburgas.bg>
2025-07-27 12:28:14 +0300

    Improve not allowed pages, now users can't access dashboard, unless approved for. Erorr pages now contain a button to go back

--- web/app/no-permission/page.tsx
+++ web/app/no-permission/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center leading-wide tracking-wide">
             <h1 className="text-white text-xl font-semibold text-center">Sorry, you're not allowed to visit this page yet.</h1>
+            <a href="/dashboard">Click <span className="underline hover:-translate-y-1 font-semibold">here</span> to go back</a>
         </div>
     )
 }
--- web/app/not-allowed/page.tsx
+++ web/app/not-allowed/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center leading-wide tracking-wide">
-            <h1 className="text-white text-xl font-semibold text-center">Sorry, you're not allowed to visit this page yet. <h1>Please ask an admin to approve your authentication request.</h1></h1>
+            <h1 className="text-white text-xl font-semibold text-center">Sorry, you're not allowed to visit this page yet. Please ask an admin to approve your authentication request.</h1>
+            <a href="/dashboard">Click <span className="underline hover:-translate-y-1 font-semibold">here</span> to go back</a>
         </div>
     )
 }
